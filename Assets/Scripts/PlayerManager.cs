ee1381ade5df251f2e3d34513a710173385426f8
Sabine M. Heyne <hello@smheyne.de>
2023-07-17 13:24:09 +0300

    WIP: light on touched tools for better UX

--- Assets/Scripts/PlayerManager.cs
+++ Assets/Scripts/PlayerManager.cs
@@ -1,3 +1,4 @@
+using System.Security.Cryptography;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -15,6 +16,11 @@ public class PlayerManager : MonoBehaviour
     private bool showingToolUI = false;
     public float timeRemainingOnCountdown = 3;
     public GameObject useToolUI;
+
+    public string toolInSpotlight;
+    bool isWateringCanEnlightened = false;
+    public GameObject lightOnWateringCan;
+    public GameObject lightOnPickaxe;
 
     // Start is called before the first frame update
     void Start()
@@ -40,6 +46,26 @@ public class PlayerManager : MonoBehaviour
         if (showingToolUI)
         {
             StartCountdownToDisableToolUI();
+
+            lightOnWateringCan.SetActive(isWateringCanEnlightened);
+                        
+        }
+        
+        if (toolInSpotlight == "WateringCan" && showingToolUI)
+        {
+            isWateringCanEnlightened = true;
+        }   
+        else
+        {
+            isWateringCanEnlightened = false;
+        }
+
+
+        
+        if(showingToolUI && Input.GetKeyDown(KeyCode.E))
+        {
+            Debug.Log("Key E was pressed!");
+            //ChangePlayerAppearance();
         }
     }
 
@@ -56,11 +82,17 @@ public class PlayerManager : MonoBehaviour
         if(collision.gameObject.name.Equals("WateringCan")){
             Debug.Log("touched wateringCan");
             SetToolUIVisibility(toolUiIsVisible: true);
+
+                toolInSpotlight = "WateringCan";
+                Debug.Log("Name for the tool touched: " + toolInSpotlight);
         }
 
         if(collision.gameObject.name.Equals("Pickaxe")){
             Debug.Log("touched Pickaxe");
-            SetToolUIVisibility(toolUiIsVisible: true);   
+            SetToolUIVisibility(toolUiIsVisible: true);
+
+                toolInSpotlight = "Pickaxe";
+                Debug.Log("Name for the tool touched: " + toolInSpotlight);
         }
     }
 
@@ -68,6 +100,7 @@ public class PlayerManager : MonoBehaviour
     {
         showingToolUI = toolUiIsVisible;
         useToolUI.gameObject.SetActive(toolUiIsVisible);
+        timeRemainingOnCountdown = 3;
     } 
 
     private void StartCountdownToDisableToolUI()
@@ -80,4 +113,11 @@ public class PlayerManager : MonoBehaviour
             timeRemainingOnCountdown = 3;
         }
     }
+
+    /*
+    private void ChangePlayerAppearance()
+    {
+
+    }
+    */
 }
